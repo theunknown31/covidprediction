965b9e684614b378a8d54cc5331096f5e2b6dd99
Pranjal Varshney <pranjal31var@gmail.com>
2020-06-08 13:31:28 +0530

    k

--- myscript.js
+++ myscript.js
@@ -185,10 +185,10 @@ request.onload = function() {
 if(obj["in"]==null||obj["in"]==undefined)
 {
 obj["in"]=obj["tt"];    
-obj["in"][0]+=obj["un"][0]+obj["un"][0];
-obj["in"][1]+=obj["un"][1]+obj["un"][1];
-obj["in"][2]+=obj["un"][2]+obj["un"][2];
-    obj["in"][3]+=obj["un"][3]+obj["un"][3];
+obj["in"][0]+=obj["un"][0]];
+obj["in"][1]+=obj["un"][1];
+obj["in"][2]+=obj["un"][2];
+    obj["in"][3]+=obj["un"][3];
 console.log(india)
 change({
 "srcElement":{
